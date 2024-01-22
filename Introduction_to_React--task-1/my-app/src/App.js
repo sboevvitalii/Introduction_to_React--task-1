d1d6247ec7beaea9de89a54d4108088660f05326
sboev.vitalii <sboev.vitalii@mail.ru>
2024-01-22 15:52:30 +0600

    добавил текуший год при помомщи JavaScript

--- Introduction_to_React--task-1/my-app/src/App.js
+++ Introduction_to_React--task-1/my-app/src/App.js
@@ -2,28 +2,24 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.2
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <div>
-          <h2>Сегодня, {createYear.getFullYear()} год!</h2>
-        </div>
-      </header>
-      <footer></footer>
-    </div>
-  );
+  let formInnerHTML = `
+	<div class="App">
+		<header class="App-header">
+			<img src=${logo} class="App-logo" alt="logo" >
+			<p>
+			Edit <code>src/App.js</code> and save to reload.2
+			</p>
+			<a class="App-link href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+			Learn React
+			</a>
+		<div>
+				<h2>Сегодня, ${createYear.getFullYear()} год!</h2>
+		</div>
+		</header>
+		<footer></footer>
+	</div>
+	`;
+  document.body.innerHTML += formInnerHTML;
 }
 const createYear = new Date();
 export default App;
