f01afa9bb968b23ed0c239ed0b6b51d1aa18c032
sboev.vitalii <sboev.vitalii@mail.ru>
2024-01-22 15:46:36 +0600

    добавил текуший год при помомщи jSX

--- Introduction_to_React--task-1/my-app/src/App.js
+++ Introduction_to_React--task-1/my-app/src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.2
         </p>
         <a
           className="App-link"
@@ -17,9 +17,13 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+          <h2>Сегодня, {createYear.getFullYear()} год!</h2>
+        </div>
       </header>
+      <footer></footer>
     </div>
   );
 }
-
+const createYear = new Date();
 export default App;
